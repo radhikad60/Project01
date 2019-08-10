8d6de85624742281c637e5103c2a8ac2d39e12c2
radhikad60 <radhikad60@gmail.com>
2019-08-10 17:23:53 +0530

    created new branch

--- Fun_2.py
+++ Fun_2.py
@@ -1,9 +1,12 @@
-def Gretest(num1,num2):
+def gretest(num1,num2):
     if num1>num2:
-        print("gretest number is "%num1")
-    else
-        print(f "greteset number is"%num2)
+        print("gretest number is %d" % num1)
+    else:
+        print("greteset number is %d" % num2)
+    return    
 
- x=int(input("plese enter first number"))
- y=int(input("please enter second number"))
- z=Gretest(x,y)      
+x=int(input("plese enter first number"))
+y=int(input("please enter second number"))
+
+gretest(x,y)    
+
